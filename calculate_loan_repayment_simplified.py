3db6a7737c2740303a03d18389d1777de674b117
chayanit.ru <chayanit.ru@tnis.com>
2022-12-09 18:51:47 +0700

    Add logs in calculate_loan_repayment_simplified.py

--- calculate_loan_repayment_simplified.py
+++ calculate_loan_repayment_simplified.py
@@ -1,6 +1,7 @@
 import os
 import sys
 from utilities import *
+from datetime import datetime
 from pandas.tseries.offsets import DateOffset
 from loan_repayment_simplified_calculation.dlor_with_loan_details_preparation import *
 from loan_repayment_simplified_calculation.installment_period_preparation import *
@@ -11,6 +12,8 @@ from loan_repayment_simplified_calculation.loan_payment_label_preparation import
 from loan_repayment_simplified_calculation.loan_repayment_simplified_preparation import *
 from loan_repayment_simplified_calculation.loan_repayment_simplified_concatenation import *
 
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger("LOAN_REPAYMENT_SIMPLIFIED")
 
 def main():
 
@@ -33,6 +36,8 @@ def main():
     multiple_loan_simplified_path = config['multiple_simplified_path']
     multiple_loan_simplified_output_path = "{}{}.tsv".format(config['multiple_loan_simplified_output_path'],today_date.strftime("%Y%m%d"))
 
+    log.info("Loan Repayment Simplified Calculation (Start): {}".format(datetime.now().strftime("%d/%m/%Y %H:%M:%S")))
+
     dlor_with_loan_product_details = prepare_dlor_with_loan_product_details(loan_product_details_path,
                                                                             daily_dlor_path,
                                                                             product_name)
@@ -54,6 +59,9 @@ def main():
     dlor_daily_update.to_csv(dlor_daily_output_path, sep='\t', index=False)
 
     concat_loan_repayment(multiple_loan_simplified_path, multiple_loan_simplified_output_path)
+
+    log.info("Loan Repayment Simplified Calculation (Finish): {}".format(datetime.now().strftime("%d/%m/%Y %H:%M:%S")))
+
 
 if __name__ == "__main__":
     main()
